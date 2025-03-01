01b88c2918ad1e8a5b7583e3f9e529dc890e7b32
ronit-1404 <88834510+ronit-1404@users.noreply.github.com>
2025-03-01 14:44:53 +0530

    bloodrequest route and api created

--- backend/controllers/bloodRequestController.js
+++ backend/controllers/bloodRequestController.js
@@ -28,3 +28,18 @@ exports.getRequests = async (req,res) => {
         res.status(500).json({ error: error.message });      
     }
 }
+
+exports.deleteRequests = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const request = await BloodRequest.findByIdAndDelete(id);
+
+        if (!request) {
+            return res.status(404).json({ success: false, message: "Request not found" });
+        }
+
+        res.status(200).json({ success: true, message: "Blood request deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
+};
