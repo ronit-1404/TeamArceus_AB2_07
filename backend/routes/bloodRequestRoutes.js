0642b333b9250a3525351d8f21d3adebc75364db
ronit-1404 <88834510+ronit-1404@users.noreply.github.com>
2025-03-02 09:44:59 +0530

    bloodrequestroutes corrected

--- backend/routes/bloodRequestRoutes.js
+++ backend/routes/bloodRequestRoutes.js
@@ -1,11 +1,11 @@
 const express =require("express")
-
 const {getRequests,deleteRequests} = require("../controllers/bloodRequestController.js")
-
+const {createRequest} = require("../controllers/bloodRequestController.js")
 
 const router = express.Router();
 
 router.get('/servicerequest',getRequests)
 router.delete('/:id',deleteRequests);
+router.post("/request-blood",createRequest)
 
 module.exports = router;
